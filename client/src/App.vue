cd3cc6f76960cba284f5ca359414aa7be0671dd5
ACERY1 <y841034081@gmail.com>
2017-09-02 17:29:07 +0800

    fix bug in html-> vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,13 +19,16 @@
 		  /*TODO:根据session 里是否有temp_courseId 来判断是否是从制作课件跳转回来的*/
 				if (getSession("temp_courseId") != null) {
 					this.$router.push('/static/classInfo')
+					return;
 				}
 				if(getSession('courseId_forClass')){
 					// 上课阶段防止跳转
 					this.$message("正在上课呢")
+				  	return;
 				}
 				else {
 					this.$router.push('/static/main')
+				  	return;
 				}
 
 			}
